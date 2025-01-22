3fe2c6e0ab5b3dd23e238d3abadbd042de70b062
fb0801 <fbhatti0801@gmail.com>
2025-01-22 17:43:20 +0000

    implement store api 1.1

--- store/src/app/pages/home/home.component.ts
+++ store/src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { Product } from '../../models/product.model';
+import { Subscription } from 'rxjs';
+import { StoreService } from '../../services/store.service';
 
 const ROWS_HEIGHT: { [id:number]: number} = {1:400, 3:355, 4:350}
 
@@ -12,12 +14,24 @@ export class HomeComponent implements OnInit{
   cols = 3;
   rowHeight = ROWS_HEIGHT[this.cols]
   category: string | undefined
+  products: Array<Product> | undefined
+  sort = 'desc'
+  count = '12'
+  productSubscription: Subscription | undefined
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService, private storeService: StoreService) { }
 
   ngOnInit(): void {
+    this.getProducts()
       
   }
+
+getProducts() : void {
+  this.storeService.getAllProducts(this.count, this.sort)
+  .subscribe((_products) => {
+    this.products = _products
+  })
+}
 
 onColumnsCountChange(colsNum: number): void {
   this.cols= colsNum;
